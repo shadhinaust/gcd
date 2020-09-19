1469830b647f63ae9ff262bd573a293dd0fa3a5e
shadhin.aust <shadhin.aust@gmail.com>
2020-09-19 12:19:10 +0600

    added google login config

---
+++ src/main/java/com/mohit/gcd/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.mohit.gcd.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**").authorizeRequests()
+                .antMatchers(new String[]{"/"}).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
+}
--- src/main/java/com/mohit/gcd/controller/LoginController.java
+++ src/main/java/com/mohit/gcd/controller/LoginController.java
@@ -3,11 +3,23 @@ package com.mohit.gcd.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class LoginController {
 
     @GetMapping("")
-    public String login() {
-        return "Hi, gcd";
+    public String welcom() {
+        return "Good day, Please login";
+    }
+
+    @GetMapping("/restricted")
+    public Principal login(Principal Principal) {
+        return Principal;
+    }
+
+    @GetMapping("welcome")
+    public String google() {
+        return "I am authenticated by google";
     }
 }
