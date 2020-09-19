4fdfbbfaea886eae0c232cb7410925420df7a09f
shadhin.aust <shadhin.aust@gmail.com>
2020-09-19 11:19:37 +0600

    removed default login and added sample controller

--- src/main/java/com/mohit/gcd/GcdApplication.java
+++ src/main/java/com/mohit/gcd/GcdApplication.java
@@ -2,8 +2,9 @@ package com.mohit.gcd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class GcdApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GcdApplication.class, args);
